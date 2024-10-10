0b8b8ef19d026d58d9730ef5c8d13111820afcfe
scm32482 <matthias.schoen@st.oth-regensburg.de>
2024-10-10 15:49:59 +0200

    .

--- DelayAndSum/DelayAndSumTB.cpp
+++ DelayAndSum/DelayAndSumTB.cpp
@@ -1,5 +1,11 @@
 #include "DelayAndSum.hpp"
+#include <iostream> 
+#include <fstream>
 
 int main(){
     int ret = 0;
+
+    std::ifstream fs("test_fstream.txt", std::ifstream::in);
+    
+    return ret;
 }
